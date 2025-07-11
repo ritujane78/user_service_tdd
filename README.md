827d269911d2a2ed30c23db66c852cb88565a961
Ritu Bafna <ritujane78@gmail.com>
2025-07-11 08:54:48 +0545

    remove redundancy and add DisplayName annotation

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ The unit test is written first for createUser method.
 The wrongs in the unit test are fixed line by line by implementing the method, 
 making the red turn to green for each step and finally the unit test is passed.
 
+The redundancy is removed by making a single test method and in that unit test, DisplayName annotation is also added.
+
--- src/test/java/com/jane/estore/service/UserServiceTest.java
+++ src/test/java/com/jane/estore/service/UserServiceTest.java
@@ -2,6 +2,7 @@ package com.jane.estore.service;
 
 import com.jane.estore.model.User;
 import com.jane.estore.service.UserService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -9,6 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class UserServiceTest {
 
+    @DisplayName("User Object Created")
     @Test
     void teatCreateUser_whenUserDetailsProvided_returnUserProject() {
 //        Arrange
@@ -23,22 +25,7 @@ public class UserServiceTest {
 
 //         Assert
         assertNotNull(user,"The createUserr() should not have returnd null");
-    }
-
-    @Test
-    void testCreateUser_WhenUserCreated_ReturnUserObjectContainsSameFirstName(){
-//        Arrange
-        UserService userService = new UserServiceImpl();
-        String firstName = "Ritu";
-        String lastName = "Bafna";
-        String email = "ritujane78@gmail.com";
-        String password = "test123";
-        String confirmPassword = "test123";
-
-//        Act
-        User user = userService.createUser(firstName,lastName,email,password,confirmPassword);
-
-//        Assert
         assertEquals(firstName,user.getFirstName(),"FirstName of the user is incorrect.");
     }
+
 }
